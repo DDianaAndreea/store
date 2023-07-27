201f729a9052fca918e7eb26ae5986c9d176bc05
DDianaAndreea <dyusk_96@yahoo.com>
2023-07-27 20:10:12 +0300

    add the implementation of cart logic

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,Input ,Output,EventEmitter} from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-box',
@@ -7,9 +8,23 @@ import { Component, OnInit,Input } from '@angular/core';
 })
 export class ProductBoxComponent implements OnInit {
   @Input() fullWidthMode=false;
+  product:Product |undefined={
+    id:1,
+    title:'Shoes',
+    price:150,
+    category: 'shoes',
+    desciption:'decription',
+    image:'https://fakeimg.pl/500x500'
+  };
+
+  @Output() addToCart = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onAddToCart():void{
+    this.addToCart.emit(this.product);
   }
 
 }
